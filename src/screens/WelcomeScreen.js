b85f590b8bc49c63dc0af0ad7e5f8f174f166e0a
Denis <denis.tojaga@edu.fit.ba>
2021-05-22 12:14:58 +0200

    //welcome screen finished

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,20 +1,83 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, Text, Button, View, Image, Dimensions, ImageBackground, TouchableOpacity } from "react-native";
+
+const { width, height } = Dimensions.get("screen");
 
 
 const WelcomeScreen = ({ navigation }) => {
 
     return (
-        <View>
-            <Text>This is home screen</Text>
-            <Button title="Go to signin screen!" onPress={() => navigation.navigate("Signin")} />
-            <Button title="Go to signup screen!" onPress={() => navigation.navigate("Signup")} />
+        <View style={styles.container}>
+            <ImageBackground style={styles.backgroundImage} source={require("../images/welcome.png")}>
+                <Image style={styles.logo} source={require("../images/logo.png")} />
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Signin")}>
+                    <Text style={styles.buttonText}>Sign in</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Signup")}>
+                    <Text style={styles.buttonText}>Sign up</Text>
+                </TouchableOpacity>
+
+            </ImageBackground>
         </View>
     );
 };
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+
+    backgroundImage: {
+        width: width,
+        height: height,
+        opacity: .6
+    },
+
+    logo: {
+
+        width: width * 0.8,
+        height: height * .5,
+        resizeMode: "contain",
+        marginBottom: 40,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 7
+        },
+        shadowOpacity: .4,
+        shadowRadius: 17,
+    },
+
+    button: {
+        borderRadius: 20,
+        backgroundColor: "#CEB01A",
+        width: width * 0.7,
+        height: 60,
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: width * 0.15,
+        marginBottom: 20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 9
+        },
+        shadowOpacity: .9,
+        shadowRadius: 9,
+    },
+
+    buttonText: {
+        fontFamily: "TrendaRegular",
+        fontSize: 25,
+    }
+});
+
+
+WelcomeScreen.navigationOptions = {
+    headerShown: false
+}
 
 
 export default WelcomeScreen;
